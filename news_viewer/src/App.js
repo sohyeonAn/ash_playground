88e99321aa4343f595c1fb826607378df75fb9bc
sohyeonAn <ansh4393@gmail.com>
2022-02-15 14:37:15 +0900

    🎉 news_viewer 최초 커밋

---
+++ news_viewer/src/App.js
@@ -0,0 +1,16 @@
+import { useCallback, useState } from 'react';
+import Categories from './components/Categories';
+import NewsList from './components/NewsList';
+
+function App() {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
+}
+
+export default App;
---
+++ news_viewer/src/components/NewsList.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 786px;
+  margin: 2rem auto 0;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding: 0 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=4741816cf3ad4cb18c6a2d870d7dcbd7`,
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+  // 대기 중일 때
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+
+  // 아직 articles 값이 설정되지 않았을 때
+  if (!articles) {
+    return null;
+  }
+
+  // articles 값이 유효할 때
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
